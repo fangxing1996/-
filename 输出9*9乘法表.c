4654352d1af0bf4d24c5ea3e4ba051db1eabcc00
方行 <fangxing1996@126.com>
2020-03-15 17:05:12 +0800

    Create 输出9*9乘法表.c

---
+++ 输出9*9乘法表.c
@@ -0,0 +1,15 @@
+/*输出9*9乘法表*/
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	for(i = 1; i < 10; i++)
+	{
+		for(j = 1; j <= i; j++)
+		{
+			printf("%d*%d = %d ", j, i, i*j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
